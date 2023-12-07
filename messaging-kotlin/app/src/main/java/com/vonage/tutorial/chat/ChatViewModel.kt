087d97f5d0ea80161afe7056a4763a74901cc041
Zachary Powell <zachary.powell@vonage.com>
2023-12-07 14:39:46 +0000

    Fixes for chat view

--- messaging-kotlin/app/src/main/java/com/vonage/tutorial/chat/ChatViewModel.kt
+++ messaging-kotlin/app/src/main/java/com/vonage/tutorial/chat/ChatViewModel.kt
@@ -19,9 +19,9 @@ import com.vonage.clientcore.core.api.models.PersistentConversationEvent
 import com.vonage.clientcore.core.api.models.PresentingOrder
 
 class ChatViewModel(application: Application) : AndroidViewModel(application = application) {
-    private val aliceJwt = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3MDEzNTE3MTksImp0aSI6IjNjNGE4MzkwLThmODYtMTFlZS1hOTNhLTkxZDk3OGRmMzYwNCIsImFwcGxpY2F0aW9uX2lkIjoiOTFhMDhiYzgtYzAyZi00NmJiLWExMTktNzA5NjMxMmYxZDRhIiwic3ViIjoiQWxpY2UiLCJleHAiOjE3MDEzNTE3NDEwOTcsImFjbCI6eyJwYXRocyI6eyIvKi9jb252ZXJzYXRpb25zLyoqIjp7fSwiLyovc2Vzc2lvbnMvKioiOnt9LCIvKi9kZXZpY2VzLyoqIjp7fSwiLyovcHVzaC8qKiI6e30sIi8qL2tub2NraW5nLyoqIjp7fSwiLyovbGVncy8qKiI6e319fX0.mbs5RVB8OHPZ09lSCSot4D0eMYmO_FgoQUfIvB5epzULrhK3KLvyJiR4n_y6ZOsjEpwk3ILfmzW_X5ukFebxv1xepOFQ4dRc1kgbn0nXWPnjjBEP4zODl9YbGyWiv0B0ydoNLXV8nFrTHBjUjGmTbXSdmvvl41db-8BDYMOwE-1SqiCZVz8l8xAlXFQMv_tkymra_2RxTNdmxqgnDOXIgv4PsbVUwLgzvdTxSRK8-P_ZpoZcsx_LMOKWFMRH1tK0l83OZp9tjrk6wszr1LGJAS2k8ESbkEAkAY-QNvsDTQhiDASOVdB2Nkiv9tORw9Rn_QonLa9eY40nYWz3E5uA1Q" //Set to ALICE JWT
-    private val bobJwt = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3MDEzNTE3NDksImp0aSI6IjRlMTRjZTAwLThmODYtMTFlZS05NGU2LTJkNzE3Y2E5MzEwZSIsImFwcGxpY2F0aW9uX2lkIjoiOTFhMDhiYzgtYzAyZi00NmJiLWExMTktNzA5NjMxMmYxZDRhIiwic3ViIjoiQm9iIiwiZXhwIjoxNzAxMzUxNzcwOTQ0LCJhY2wiOnsicGF0aHMiOnsiLyovY29udmVyc2F0aW9ucy8qKiI6e30sIi8qL3Nlc3Npb25zLyoqIjp7fSwiLyovZGV2aWNlcy8qKiI6e30sIi8qL3B1c2gvKioiOnt9LCIvKi9rbm9ja2luZy8qKiI6e30sIi8qL2xlZ3MvKioiOnt9fX19.jqOwYWeN02R_3XPPTuxShEMAHOq-ZakxlqSLCLXnntUxL_GnO8J6eNKC1ATpLW5g_M1jjbNOY8krCezAwKeMsG2aazHOlfpPkNClSiIFVpLwUdswHRKV94kLxjH6wmwZ_c48u_ZopxgnHD0LgX_L51XFW8YhqWITqA98l5i5bJwg73OhwTopxYJrLr08EBleIyYIS7Ytm11-_P17g6Bbe6iydETzErnom6u7qU0HxeyPzmj9JM-pEY7DjOrlKBdV1_vBJj6HDTZM5VulNNqdr-2oyCZ1eH-keWZIbMntmNlYH3ix-d7wNEB5B3r8ShvizQY7p0IjQERaS1koTqXQfw" //Set to BOB JWT
-    private val conversationID = "CON-7be0ac0f-5309-4418-970b-f476cc7e232a" //Set to created conversation ID that has Bob and Alice as members
+    private val aliceJwt = "" //Set to ALICE JWT
+    private val bobJwt = "" //Set to BOB JWT
+    private val conversationID = "" //Set to created conversation ID that has Bob and Alice as members
 
     var isLoggedIn by mutableStateOf(false)
     var isError by mutableStateOf(false)
@@ -73,7 +73,8 @@ class ChatViewModel(application: Application) : AndroidViewModel(application = a
         val params = GetConversationEventsParameters(PresentingOrder.ASC,100)
         try {
             val eventsPage = client.getConversationEvents(conversationID, params)
-            events = eventsPage.events.toMutableStateList()
+            events.clear()
+            events.addAll(eventsPage.events.toMutableStateList())
         }
         catch (err:Error) {
             isError = true
--- messaging-kotlin/app/src/main/java/com/vonage/tutorial/chat/MainActivity.kt
+++ messaging-kotlin/app/src/main/java/com/vonage/tutorial/chat/MainActivity.kt
@@ -10,7 +10,9 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -32,8 +34,13 @@ import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.AndroidViewModel
 import com.vonage.android_core.VGError
 import com.vonage.chat.ChatClient
@@ -105,17 +112,19 @@ fun ChatScreen() {
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Bottom
     ) {
-        if (vm.events.isEmpty()){
-            CircularProgressIndicator(
-                modifier = Modifier.width(64.dp).height(300.dp),
-                color = MaterialTheme.colorScheme.secondary,
-            )
-        } else {
-            LazyColumn() {
-                items(vm.events) { event ->
-                    val (text, isUser) = vm.generateDisplayedText(event)
-                    Text(text)
-                }
+        LazyColumn(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            items(vm.events) { event ->
+                val (text, isUser) = vm.generateDisplayedText(event)
+                val textAlightment = if (isUser) TextAlign.Right else TextAlign.Left
+                val textColor = if (isUser) Color.Blue else Color.Black
+                Text(
+                    text = text,
+                    fontSize = 20.sp,
+                    textAlign = textAlightment,
+                    color = textColor
+                )
             }
         }
         Row(){
